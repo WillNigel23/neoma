67fa800d409662e087f83de6e28193cbdc20cc7e
WillNigel23 <wcjdesus@up.edu.ph>
2024-02-02 18:01:47 +0800

    Navbar implemented

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,54 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="navbar"
+export default class extends Controller {
+    static targets = ["nav", "menu", "grunge"]
+
+    connect() {
+        this.navShown = true;
+        this.menuShown = false;
+        this.scrollCount = 0;
+        document.addEventListener('wheel', (event) => this.handleWheel(event));
+    }
+
+    handleWheel(event) {
+        if (event.deltaY > 0) {
+            this.scrollCount++;
+
+            console.log(this.scrollCount);
+            if (this.scrollCount > 2) {
+                if (this.navShown) {
+                    this.navShown = !this.navShown;
+                    this.changeState(this.navShown, 'y', this.navTarget);
+                }
+                this.scrollCount = 0;
+            }
+        }
+        else if (event.deltaY < 0) {
+            if (!this.navShown) {
+                this.navShown = !this.navShown;
+                this.changeState(this.navShown, 'y', this.navTarget);
+            }
+            this.scrollCount = 0;
+        }
+    }
+
+    toggleMenu() {
+        this.menuShown = this.navShown;
+        this.navShown = !this.navShown;
+
+        this.changeState(this.navShown, 'y', this.navTarget);
+        setTimeout(() => {
+            this.changeState(this.menuShown, 'x', this.menuTarget);
+        }, 250);
+    }
+
+    changeState(curState, direction, curTarget) {
+        if (curState) {
+            curTarget.classList.remove(`-translate-${direction}-full`);
+        }
+        else {
+            curTarget.classList.add(`-translate-${direction}-full`);
+        }
+    }
+}
