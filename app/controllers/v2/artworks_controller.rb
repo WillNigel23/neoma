2874d54538db2655e689e31848e58c46beb108a3
WillNigel23 <willnigelcdejesus@gmail.com>
2024-05-14 23:41:41 +0800

    Added artwork modal implementation for V2

--- app/controllers/v2/artworks_controller.rb
+++ app/controllers/v2/artworks_controller.rb
@@ -2,13 +2,39 @@ module V2
   class ArtworksController < ApplicationController
 
     def index
-      @artworks = Artwork.live.order(year: :desc)
+      @artworks = Artwork.live.includes(:artist).order(year: :desc)
       @artwork_ids = @artworks.to_a.pluck(:id)
     end
 
     def show
-      @artwork = Artwork.includes(:artist).friendly.find(params[:id])
-      @artwork_ids = params[:artwork_ids].presence || Artwork.live.pluck(:id)
+      @reference_id = params[:reference_id]
+      @class_name = params[:class_name]
+
+      @artworks = artworks_by_class_reference(@reference_id, @class_name)
+
+      @position = @artworks.find_index { |artwork| artwork.slug == params[:id] } || 0
+
+      respond_to do |format|
+        format.turbo_stream
+        format.html
+      end
+    end
+
+    private
+
+    def artworks_by_class_reference(reference_id, class_name)
+      case class_name
+      when 'artist'
+        artworks = Artist.friendly.find(reference_id).artworks.live.includes(:artist)
+      when 'exhibit'
+        artworks = Exhibit.friendly.find(reference_id).artworks.live.includes(:artist)
+      when 'gallery'
+        artworks = Gallery.friendly.find(reference_id).artworks.live.includes(:artist)
+      else
+        artworks = Artwork.live.includes(:artist)
+      end
+
+      artworks
     end
 
   end
